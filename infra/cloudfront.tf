61deb31fbbd0c82a5498fcd856945ebadc11be11
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2026-01-06 11:07:04 +0900

    Add admin frontend S3 distribution

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -17,6 +17,18 @@ resource "aws_acm_certificate" "frontend" {
   tags = local.common_tags
 }
 
+resource "aws_acm_certificate" "admin_frontend" {
+  provider          = aws.use1
+  domain_name       = "${var.admin_subdomain}.${var.root_domain}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = local.common_tags
+}
+
 ########################
 # Route53 - ACM Validation
 ########################
@@ -45,11 +57,43 @@ resource "aws_acm_certificate_validation" "frontend" {
   validation_record_fqdns = [for r in aws_route53_record.frontend_acm_validation : r.fqdn]
 }
 
+resource "aws_route53_record" "admin_frontend_acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.admin_frontend.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+
+  allow_overwrite = true
+}
+
+resource "aws_acm_certificate_validation" "admin_frontend" {
+  provider                = aws.use1
+  certificate_arn         = aws_acm_certificate.admin_frontend.arn
+  validation_record_fqdns = [for r in aws_route53_record.admin_frontend_acm_validation : r.fqdn]
+}
+
 ########################
 # CloudFront OAC
 ########################
 resource "aws_cloudfront_origin_access_control" "frontend" {
   name                              = "${local.name_prefix}-oac-frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_origin_access_control" "admin_frontend" {
+  name                              = "${local.name_prefix}-oac-admin-frontend"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
@@ -107,6 +151,54 @@ resource "aws_cloudfront_distribution" "frontend" {
   tags = local.common_tags
 }
 
+resource "aws_cloudfront_distribution" "admin_frontend" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [
+    "${var.admin_subdomain}.${var.root_domain}"
+  ]
+
+  origin {
+    domain_name              = aws_s3_bucket.admin_frontend.bucket_regional_domain_name
+    origin_id                = "s3-admin-frontend"
+    origin_access_control_id = aws_cloudfront_origin_access_control.admin_frontend.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-admin-frontend"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.admin_frontend.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  tags = local.common_tags
+}
+
 
 ########################
 # S3 Bucket Policy: CloudFront(OAC)만 읽기 허용
@@ -134,4 +226,29 @@ data "aws_iam_policy_document" "frontend_bucket" {
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   policy = data.aws_iam_policy_document.frontend_bucket.json
+}
+
+data "aws_iam_policy_document" "admin_frontend_bucket" {
+  statement {
+    sid     = "AllowCloudFrontRead"
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.admin_frontend.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.admin_frontend.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "admin_frontend" {
+  bucket = aws_s3_bucket.admin_frontend.id
+  policy = data.aws_iam_policy_document.admin_frontend_bucket.json
 }

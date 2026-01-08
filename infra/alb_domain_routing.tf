386a8629227a4a6c82c19807aa0c63453174e0cf
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2026-01-08 15:13:50 +0900

    Normalize ALB routing file line endings

--- infra/alb_domain_routing.tf
+++ infra/alb_domain_routing.tf
@@ -2,11 +2,10 @@
 # ACM for ALB (Seoul, ap-northeast-2)
 ########################
 resource "aws_acm_certificate" "alb" {
-  domain_name       = var.root_domain
+  domain_name       = "${var.api_subdomain}.${var.root_domain}"
   validation_method = "DNS"
 
   subject_alternative_names = [
-    "${var.api_subdomain}.${var.root_domain}",
     "${var.jenkins_subdomain}.${var.root_domain}",
     "${var.grafana_subdomain}.${var.root_domain}",
   ]
